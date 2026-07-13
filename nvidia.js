4a318dc1be4c0da264e4c0bfc71831751d5244d3
warrendectraff-tech <warrendectraff@gmail.com>
2026-07-13 06:37:07 +0530

    Update nvidia.js

--- nvidia.js
+++ nvidia.js
@@ -54,10 +54,6 @@ $(document).ready(function() {
     })
 }), addEvent(document, "mouseout", function(e) {
     null == e.toElement && null == e.relatedTarget && $(".lightbox").slideDown()
-}), $("a.close").click(function() {
-    $(".lightbox").slideUp()
-}), $("body").click(function() {
-    $(".lightbox").slideUp()
 }), window.addEventListener("beforeunload", function(e) {
     var n = "It looks like you have been editing something. If you leave before saving, your changes will be lost.";
     return (e || window.event).returnValue = n, n
